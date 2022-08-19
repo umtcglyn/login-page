d2af4448630db8ffc4c779c773c63db4433d2c16
Umut Çağlayan <umutcaglayan69@gmail.com>
2022-08-19 23:27:37 +0300

    ilk gönderme

---
+++ src/components/Home/Sidebar.vue
@@ -0,0 +1,18 @@
+<template>
+    <p class="home-page">İLK LOGİN PAGE SAYFAMA HOŞ GELDİNİZ</p>
+</template>
+
+<style>
+.home-page {
+    margin-top: 50px;
+    text-align: center;
+    position:absolute;
+    top:50%;
+    left:50%;
+    width:200px;
+    height:200px;
+    background-color:#ff6700;
+    transform:translate(-50%,-50%);
+    -webki-transform:translate(-50%,-50%)
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import router from './router';
+import App from './App.vue';
+import "@/assets/style.css"
+import store from './store';
+import { appAxios } from './utils/appAxios';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+app.mount("#app");
